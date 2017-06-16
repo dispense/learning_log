db8860c6dd41e80e3614e830009c474c39551def
dispense <rburn3@uis.edu>
2017-06-16 12:33:06 -0500

    Entry Function Working
    Completed function to add entry under topics

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -38,21 +38,21 @@ def new_topic(request):
 	context = {'form': form}
 	return render(request, 'learning_logs/new_topic.html', context)
 
-def new_entry(request, topic_id):
-	"""Add a new entry for a particular topic"""
-	topic = Topic.objects.get(id=topic_id)
 
+def new_entry(request, topic_id):
+	"""Add a new entry for a particular topic."""
+	topic = Topic.objects.get(id=topic_id)
+	
 	if request.method != 'POST':
-		#No data submitted; create blank form
+		# No data submitted; create a blank form.
 		form = EntryForm()
 	else:
-		# POST data submitted; process data
+		# POST data submitted; process data.
 		form = EntryForm(data=request.POST)
 		if form.is_valid():
 			new_entry = form.save(commit=False)
 			new_entry.topic = topic
 			new_entry.save()
 			return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic_id]))
-
-	context = {'topic': topic, 'form' = form}
+	context = {'topic': topic, 'form': form}
 	return render(request, 'learning_logs/new_entry.html', context)
