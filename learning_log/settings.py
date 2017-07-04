4052fa8d7548757143d86a7ac008b4a7764177cc
dispense <rburn3@uis.edu>
2017-07-04 08:08:28 -0500

    Styled and started deployment
    Finished text, starting process to deploy.

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Third Party Apps
+    'bootstrap3',
+
     # My apps
     'learning_logs',
     'users',
@@ -125,3 +128,28 @@ STATIC_URL = '/static/'
 
 # My Settings
 LOGIN_URL='/users/login/'
+
+# Settings for django-bootstrap3
+BOOTSTRAP3 = {
+    'include_jquery': True,
+	}
+
+# Heroku settings
+if os.getcwd() == '/app':
+ 	import dj_database_url
+	DATABASES = {
+		'default': dj_database_url.config(default='postgres://localhost')
+	}
+
+	# Honor the 'X-Forwarded-Proto' header for request.is_secure().
+	SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+	# Allow all host headers.
+	ALLOWED_HOSTS = ['*']
+
+	# Static asset configuration
+	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+	STATIC_ROOT = 'staticfiles'
+	STATICFILES_DIRS = (
+		os.path.join(BASE_DIR, 'static'),
+	)
